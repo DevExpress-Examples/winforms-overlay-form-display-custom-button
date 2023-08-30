5472de0ced1f0743c8b7d03bbd8b682e0e06d808
DevExpressExampleBot <fakeEmail>
2023-08-30 13:02:42 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,11 +1,7 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
 Imports System.Drawing.Text
-Imports System.Linq
-Imports System.Runtime.InteropServices
 Imports System.Threading.Tasks
 Imports DevExpress.XtraBars
 Imports DevExpress.XtraBars.Ribbon
@@ -13,63 +9,69 @@ Imports DevExpress.XtraEditors
 Imports DevExpress.XtraSplashScreen
 
 Namespace DxSample
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits RibbonForm
 
         Private taskHelper As TaskHelper
+
         Private buttonImage As Image
+
         Private hotButtonImage As Image
 
         Private overlayLabel As OverlayTextPainter
+
         Private overlayButton As OverlayImagePainter
 
         Public Sub New()
-            Me.buttonImage = CreateButtonImage()
-            Me.hotButtonImage = CreateHotButtonImage()
-            Me.overlayLabel = New OverlayTextPainter()
-            Me.overlayButton = New OverlayImagePainter(buttonImage, hotButtonImage, AddressOf OnCancelButtonClick)
+            buttonImage = CreateButtonImage()
+            hotButtonImage = CreateHotButtonImage()
+            overlayLabel = New OverlayTextPainter()
+            overlayButton = New OverlayImagePainter(buttonImage, hotButtonImage, AddressOf OnCancelButtonClick)
             InitializeComponent()
         End Sub
 
         Private Function CreateButtonImage() As Image
-            Return ImageHelper.CreateImage(My.Resources.cancel_normal)
-        End Function
-        Private Function CreateHotButtonImage() As Image
-            Return ImageHelper.CreateImage(My.Resources.cancel_active)
+            Return CreateImage(Properties.Resources.cancel_normal)
         End Function
 
-        Private Async Sub OnRunTaskItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs) Handles biRunTask.ItemClick
+        Private Function CreateHotButtonImage() As Image
+            Return CreateImage(Properties.Resources.cancel_active)
+        End Function
+
+        Private Async Sub OnRunTaskItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
             biRunTask.Enabled = False
             Dim taskResult As String
-            Dim overlayHandle As IOverlaySplashScreenHandle = SplashScreenManager.ShowOverlayForm(contentPanel, customPainter:= New OverlayWindowCompositePainter(overlayLabel, overlayButton))
+            Dim overlayHandle As IOverlaySplashScreenHandle = SplashScreenManager.ShowOverlayForm(contentPanel, customPainter:=New OverlayWindowCompositePainter(overlayLabel, overlayButton))
             Try
                 taskResult = Await RunTask()
             Finally
                 SplashScreenManager.CloseOverlayForm(overlayHandle)
                 biRunTask.Enabled = True
             End Try
+
             XtraMessageBox.Show(Me, taskResult, "Task Result")
         End Sub
 
         Private Async Function RunTask() As Task(Of String)
             Dim taskResult As String
-            Me.taskHelper = New TaskHelper()
+            taskHelper = New TaskHelper()
             Try
                 taskResult = Await taskHelper.StartTask(New Progress(Of Integer)(AddressOf OnProgressChanged))
-            Catch e1 As OperationCanceledException
+            Catch __unusedOperationCanceledException1__ As OperationCanceledException
                 taskResult = "Operation is Cancelled"
             Finally
                 taskHelper.Dispose()
                 taskHelper = Nothing
             End Try
+
             Return taskResult
         End Function
 
         Private Sub OnCancelButtonClick()
-            If taskHelper IsNot Nothing Then
-                taskHelper.Cancel()
-            End If
+            If taskHelper IsNot Nothing Then taskHelper.Cancel()
         End Sub
+
         Private Sub OnProgressChanged(ByVal value As Integer)
             overlayLabel.Text = value.ToString() & "%"
         End Sub
